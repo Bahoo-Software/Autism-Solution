128c2ec203ba9adc3beb44110b99db674b53a437
Muhammad Sheraz <90733116+Muhammadsheraz492@users.noreply.github.com>
2022-12-02 22:14:23 +0500

    Half Header Fixing

--- App.js
+++ App.js
@@ -1,105 +1,47 @@
 /** @format */
 
-import * as React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import Onboarding from './screen/Onboarding'
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Onboarding from './screen/Onboarding';
 
-import Login from './screen/Login'
-import Signup from './screen/Signup'
+import Login from './screen/Login';
+import Signup from './screen/Signup';
 
-import Forget from './screen/Forget'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import Mainscreen from './screen/Mainscreen'
-import Guide from './screen/Guide'
-import Stages from './screen/Stages'
-import Punctuation from './screen/Pronunciation'
-import Twothreeword from './screen/Twothreeword'
-import Sentencebetweenfiveword from './screen/Sentencebetweenfiveword'
-import Pronunciation from './screen/Pronunciation'
-const Stack = createNativeStackNavigator()
+import Forget from './screen/Forget';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import Mainscreen from './screen/Mainscreen';
+import Guide from './screen/Guide';
+import Stages from './screen/Stages';
+import Punctuation from './screen/Pronunciation';
+import Twothreeword from './screen/Twothreeword';
+import Sentencebetweenfiveword from './screen/Sentencebetweenfiveword';
+import Pronunciation from './screen/Pronunciation';
+const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{flex: 1}}>
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator  screenOptions={{headerShown:false}}>
+          <Stack.Screen name="Onboarding" component={Onboarding} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Signup" component={Signup} />
+          <Stack.Screen name="Forget" component={Forget} />
+          <Stack.Screen name="Mainscreen" component={Mainscreen} />
+          <Stack.Screen name="Guide" component={Guide} />
+          <Stack.Screen name="Stages" component={Stages} />
+          <Stack.Screen name="Pronunciation" component={Pronunciation} />
+          <Stack.Screen name="Twothreeword" component={Twothreeword} />
           <Stack.Screen
-            options={{
-              headerShown: false,
-            }}
-            name="Onboarding"
-            component={Onboarding}
-          />
-          <Stack.Screen
-            options={{
-              headerShown: false,
-            }}
-            name="Login"
-            component={Login}
-          />
-          
-          <Stack.Screen
-            options={{
-              headerShown: false,
-            }}
-            name="Signup"
-            component={Signup}
-          />
-          <Stack.Screen
-            options={{
-              headerShown: false,
-            }}
-            name="Forget"
-            component={Forget}
-          />
-           <Stack.Screen
-            options={{
-              headerShown: false,
-            }}
-            name="Mainscreen"
-            component={Mainscreen}
-          />
-             <Stack.Screen
-            options={{
-              headerShown: false,
-            }}
-            name="Guide"
-            component={Guide}
-          />
-             <Stack.Screen
-            options={{
-              headerShown: false,
-            }}
-            name="Stages"
-            component={Stages}
-          />
-          <Stack.Screen
-            options={{
-              headerShown: false,
-            }}
-            name="Pronunciation"
-            component={Pronunciation}
-          />
-           <Stack.Screen
-            options={{
-              headerShown: false,
-            }}
-            name="Twothreeword"
-            component={Twothreeword}
-          />
-          <Stack.Screen
-            options={{
-              headerShown: false,
-            }}
             name="Sentencebetweenfiveword"
             component={Sentencebetweenfiveword}
           />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaView>
-  )
+  );
 }
 
-export default App
+export default App;
