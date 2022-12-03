e0f239e4319082ffb1940241764b98ad70ce6883
Muhammad Sheraz <90733116+Muhammadsheraz492@users.noreply.github.com>
2022-12-03 21:42:11 +0500

    Okey Quz screen COmpleted

--- screen/Quz.js
+++ screen/Quz.js
@@ -1,16 +1,91 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import Backbtn from '../assets/Backbtn.svg'
+import {View, Text, Image, FlatList,TouchableOpacity, Pressable} from 'react-native';
+import React, { useState } from 'react';
+import Backbtn from '../assets/Backbtn.svg';
+import LoginBtn from '../Components/Loginbtn';
 const Quz = () => {
+  const Data = ['Apple', 'BApple', 'Cappple', 'DApple'];
+  const [Select,SetSelect]=useState()
   return (
-    <View  style={{flex:1,marginTop:10}}>
-        <View style={{width:"53%",left:10,flexDirection:"row",justifyContent:"space-between"}}>
+    <View style={{flex: 1, paddingTop: 10, backgroundColor: '#fff'}}>
+      <View
+        style={{
+          width: '53%',
+          left: 10,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}>
         <Backbtn />
-            <Text  style={{fontSize:20,fontWeight:"bold",color:"#000000"}}>Quiz</Text>
-        </View>
-      {/* <Text>Quz</Text> */}
+        <Text style={{fontSize: 20, fontWeight: 'bold', color: '#000000'}}>
+          Quiz
+        </Text>
+      </View>
+      <View
+        style={{alignItems: 'center', height: 300, justifyContent: 'flex-end'}}>
+        <Image source={require('../assets/Apple.png')} />
+      </View>
+      <View
+        style={{
+          width: '90%',
+          alignSelf: 'center',
+          height: 50,
+          justifyContent: 'center',
+        }}>
+        <Text style={{color: '#000000', fontSize: 18, fontWeight: 'bold'}}>
+          Question 1
+        </Text>
+      </View>
+      <FlatList
+        data={Data}
+        numColumns={2}
+        renderItem={({item,index}) => (
+          <Pressable
+            style={{
+              margin:20,
+              height: 70,
+              width: 144,
+              backgroundColor: index==Select?"#E68231":'white',
+              borderRadius: 20,
+              alignItems: 'center',
+              shadowColor: '#000',
+              shadowOffset: {
+                width: 0,
+                height: 2,
+              },
+              shadowOpacity: 0.25,
+              justifyContent: 'center',
+              shadowRadius: 4,
+              elevation: 5,
+            }}
+            onPress={()=>SetSelect(index)}
+            >
+            <Text style={{fontSize: 20, fontWeight: 'bold', color:index==Select?"#fff": '#000000'}}>
+             {index+1} Apple
+            </Text>
+          </Pressable>
+        )}
+      />
+      <TouchableOpacity
+          style={{
+            width: '100%',
+            height: 46,
+            alignItems: 'center',
+            bottom:20,
+          }}
+          onPress={() => {
+            // API();
+            navigation.navigate('Mainscreen')
+          }}
+        >
+          
+          <LoginBtn
+            color="#E68231"
+            textcolor="#fff"
+            textfontsize={18}
+            name="Next"
+          />
+        </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default Quz
+export default Quz;
