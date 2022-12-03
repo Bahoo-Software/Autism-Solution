a9fc46852245f32a607ab02f99590a425173ef06
Muhammad Sheraz <90733116+Muhammadsheraz492@users.noreply.github.com>
2022-12-03 17:29:04 +0500

    Okey Quz underworking

--- screen/Quz.js
+++ screen/Quz.js
@@ -1,10 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import Backbtn from '../assets/Backbtn.svg'
 const Quz = () => {
   return (
-    <View>
-      <Text>Quz</Text>
+    <View  style={{flex:1,marginTop:10}}>
+        <View style={{width:"53%",left:10,flexDirection:"row",justifyContent:"space-between"}}>
+        <Backbtn />
+            <Text  style={{fontSize:20,fontWeight:"bold",color:"#000000"}}>Quiz</Text>
+        </View>
+      {/* <Text>Quz</Text> */}
     </View>
   )
 }
